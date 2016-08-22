53dd7dceb2b353ba09bdd2b7a13d55535cc0e8cd
Andrew Leavitt and Juan-Carlos Medina <pair+andrew+juanca@mavenlink.com>
2016-08-22 16:44:08 -0700

    [SPIKE] Sync redux store -> brainstem storage manager for changes

--- index.js
+++ index.js
@@ -183,6 +183,39 @@ removeModel = (state, brainstemKey, attributes) => {
 
 store = createStore(reducer)
 
+store.subscribe(() => {
+  const brainstemTree = store.getState().brainstem;
+
+  for (let brainstemKey in brainstemTree) {
+    let models = brainstemTree[brainstemKey];
+
+    for (let modelId in models) {
+      let modelAttributes = models[modelId];
+
+      storageManager.storage(brainstemKey).get(modelId).set(modelAttributes)
+    }
+  }
+
+});
+
+// // REACT -> STORAGE MANAGER
+// React updates posts -> store gets updated
+// Store invokes all listeners
+// Listener tries to update all models on storage manager
+// Changed post actually updates and emits a change event
+// Change event is handled by dispatched a CHANGE_MODEL action
+// Store invokes all listeners
+// Listener tries to update all models on storage manager
+// Post does not change so backbone does not emit any event
+
+// // STORAGE MANAGER -> REACT
+// Storage manager emits a change event
+// Change event is handled by dispatching a CHANGE_MODEL action
+// Store is updated
+// Store invokes all listeners
+// Listener tries to update all models on storage manager
+// Model does not change so backbone does not emit any event
+
 collectionNames = storageManager.collectionNames()
 
 for (let collectionName of collectionNames) {
