d5c4f412c28d706302e22f990297968ed1a29a69
Andrew Leavitt and Juan-Carlos Medina <pair+andrew+juanca@mavenlink.com>
2016-08-31 15:09:03 -0600

    Implement collection action creators specs (fetch)
    preFetchAction and postFetchAction are currently not tested. This could change since custom brainstem-redux middleware might be a possible refactor

---
+++ spec/actions/collection-spec.js
@@ -0,0 +1,37 @@
+describe('collection action creators', function() {
+  beforeEach(function() {
+    this.fetch = require('../../lib/actions/collection')(this.storageManager).fetch;
+    this.storageManager.enableExpectations();
+  });
+
+  it('fetches the collection', function() {
+    const expectation = this.storageManager.stub('posts', { filters: { foo: 'bar' } });
+    this.store.dispatch(this.fetch('posts', {
+      fetchOptions: { filters: { foo: 'bar' } }
+    }));
+
+    expect(expectation.requestQueue.length).toBe(1);
+  });
+
+  describe('previous XHRs', function() {
+    it('uses a trackKey to abort previous pending XHR', function() {
+      const abort = jasmine.createSpy('abort');
+      const state = () => 'pending';
+      spyOn(this.storageManager.storage('posts'), 'fetch').and.returnValue({ abort, state });
+      this.store.dispatch(this.fetch('posts', { trackKey: 'foo' }));
+      this.store.dispatch(this.fetch('posts', { trackKey: 'foo' }));
+
+      expect(abort).toHaveBeenCalledTimes(1)
+    });
+
+    it('does not abort non-pending previous XHR', function() {
+      const abort = jasmine.createSpy('abort');
+      const state = () => 'resolved';
+      spyOn(this.storageManager.storage('posts'), 'fetch').and.returnValue({ abort, state });
+      this.store.dispatch(this.fetch('posts', { trackKey: 'foo' }));
+      this.store.dispatch(this.fetch('posts', { trackKey: 'foo' }));
+
+      expect(abort).toHaveBeenCalledTimes(0)
+    });
+  });
+});
