2a7c169cf9c10ff2ff49955aaeb4118beb584eb2
Andrew Leavitt and Juan-Carlos Medina <pair+andrew+juanca@mavenlink.com>
2016-08-31 14:28:03 -0600

    Redux store subscriber specs

---
+++ spec/sync/subscriber-spec.js
@@ -0,0 +1,34 @@
+describe('subscriber on the redux store', function() {
+  beforeEach(function() {
+    this.posts = this.storageManager.storage('posts');
+    this.store.dispatch({
+      type: 'ADD_MODEL',
+      brainstemKey: 'posts',
+      attributes: { id: 76, title: 'Hello', message: 'World!' }
+    });
+  });
+
+  it('adds new model to storageManager', function() {
+    expect(this.posts.last().toJSON()).toEqual(this.store.getState().brainstem.posts[76]);
+  });
+
+  it('changes existing attributes on a model in the storageManager', function() {
+    this.store.dispatch({
+      type: 'CHANGE_MODEL',
+      brainstemKey: 'posts',
+      attributes: { id: 76, title: 'Goodbye', message: 'Cruel World!' }
+    });
+
+    expect(this.posts.last().toJSON()).toEqual(this.store.getState().brainstem.posts[76]);
+  });
+
+  it('removes a model from the storageManager', function() {
+    this.store.dispatch({
+      type: 'REMOVE_MODEL',
+      brainstemKey: 'posts',
+      attributes: { id: 76 }
+    });
+
+    expect(this.posts.get(76)).not.toBeDefined();
+  });
+});
