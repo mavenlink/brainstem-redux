9c420a9f6e72311345f5e50459af7150d27228b9
Juan-Carlos Medina and Naomi Jacobs <pair+juanca+naomi@mavenlink.com>
2016-09-23 15:13:58 -0700

    Fix cyclical event handler -> dispatch -> event handler -> dispatch shenanigans
    Seems like handling, say, change:username will dispatch two events which undo each other. Ultimately, the redux store will not update accordingly. However, only handling the generic change event will not. ¯\_(ツ)_/¯

--- spec/sync/event-handler-spec.js
+++ spec/sync/event-handler-spec.js
@@ -11,9 +11,9 @@ describe('handling events from storageManager', function() {
   });
 
   it('listens for change and updates the store', function() {
-    this.posts.last().set({ id: 1, title: 'This is redux!', message: 'Now I know it is awesome' });
+    this.posts.last().set({ title: 'This is redux!', message: 'Now I know it is awesome' });
 
-    expect(this.store.getState().brainstem.posts[1]).toEqual(this.posts.last().toJSON());
+    expect(this.store.getState().brainstem.posts[1].title).toEqual('This is redux!');
   });
 
   it('listens for an remove event and syncs the store', function() {
