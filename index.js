2fb46d97afffa4bf04d284581a62d1a4f81c1dc6
Naomi Jacobs <pair+naomi@mavenlink.com>
2016-10-12 19:56:55 -0700

    Modify to allow input storage manager (WIP)

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-module.exports = {
-  reducer: require('./lib/reducers/index'),
-  updateStore: require('./lib/sync/update-store'),
-  updateStorageManager: require('./lib/middleware/update-storage-manager'),
-  modelActions: require('./lib/actions/model'),
-  collectionActions: require('./lib/actions/collection'),
-};
+const setStorageManager = require('./lib/sync/storage-manager').setStorageManager;
+
+function initialize(inputStorageManager) {
+  setStorageManager(inputStorageManager);
+  
+  return {
+    reducer: require('./lib/reducers/index'),
+    updateStore: require('./lib/sync/update-store'),
+    updateStorageManager: require('./lib/middleware/update-storage-manager'),
+    modelActions: require('./lib/actions/model'),
+    collectionActions: require('./lib/actions/collection'),
+  }
+}
+
+module.exports = { initialize };
