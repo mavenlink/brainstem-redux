1f7376928df6a00d22ef250005a18192e4b04690
Juan-Carlos Medina <juanca@mavenlink.com>
2016-10-26 10:50:53 -0700

    npm install --save babel-loader babel-core babel-preset-es2015 babel-plugin-transform-runtime

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,17 @@ const Path = require('path')
 module.exports = {
   entry: './api.js',
 
+  module: {
+    loaders: [ {
+      exclude: /(node_modules)/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015'],
+        plugins: ['transform-runtime'],
+      },
+    }],
+  },
+
   output: {
     path: './bin',
     filename: 'index.js',
