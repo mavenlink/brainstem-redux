f7a2fda0f4f575d35ad4c8e579494cb34ce4cd5a
Juan-Carlos Medina <juanca@mavenlink.com>
2016-10-26 10:52:08 -0700

    npm install --save-dev karma-firefox-launcher

--- karma.config.js
+++ karma.config.js
@@ -5,7 +5,7 @@ const webpackConfig = Object.assign(require('./webpack.config'), {
 
 module.exports = (config) =>
   config.set({
-    browsers: ['Chrome', 'PhantomJS'],
+    browsers: ['Chrome', 'PhantomJS', 'Firefox'],
 
     frameworks: ['jasmine'],
 
