d936f76156857a82906643ab4999ba9de2665460
Naomi Jacobs <naomi@mavenlink.com>
2017-05-09 12:39:25 -0700

    Pull out storageManagerListener into module that caches the listener for the same store

---
+++ spec/sync/storage-manager-listener-spec.js
@@ -0,0 +1,32 @@
+const getStorageManagerListener = require('./../../lib/sync/storage-manager-listener');
+
+describe('storageManagerListener', () => {
+  it('returns a function', () => {
+    expect(typeof getStorageManagerListener).toEqual('function');
+  });
+
+  describe('when a store is not passed in the first time it is invoked', () => {
+    it('throws an error', () => {
+      expect(() => getStorageManagerListener()).toThrow(new Error('You must pass in a redux store the first time you call getStorageManagerListener'));
+      expect(() => getStorageManagerListener({})).toThrow(new Error('You must pass in a redux store the first time you call getStorageManagerListener'));
+    });
+  });
+
+  describe('when a store is passed in the first time it is invoked', () => {
+    it('returns the same function every time it is invoked after that', () => {
+      const firstReturn = getStorageManagerListener({ dispatch: () => {} });
+      expect(typeof firstReturn).toEqual('function');
+      expect(getStorageManagerListener()).toBe(firstReturn);
+      expect(getStorageManagerListener()).toBe(firstReturn);
+      expect(getStorageManagerListener()).toBe(firstReturn);
+    });
+  });
+
+  describe('when a store is passed in again', () => {
+    it('overrides the original store and returns a new handler bound to the new store', () => {
+      const listenerWithFirstStore = getStorageManagerListener({ dispatch: () => {} });
+      const listenerWithOtherStore = getStorageManagerListener({ dispatch: () => {} });
+      expect(listenerWithFirstStore).not.toBe(listenerWithOtherStore);
+    });
+  });
+});
