c9c2a13d0d885866c8ae3460d819fb6d93e88a35
Ellie Day <eday@mavenlink.com>
2017-11-09 17:00:59 -0800

    Add initial adapter pattern for collection.js
    Signed-off-by: Brandon Duff <bduff@mavenlink.com>

--- spec/actions/collection-spec.js
+++ spec/actions/collection-spec.js
@@ -10,62 +10,80 @@ describe('collection action creators', () => {
     this.storageManager.enableExpectations();
   });
 
-  it('fetches the collection', function () {
-    const expectation = this.storageManager.stub('posts', { filters: { foo: 'bar' } });
-    this.store.dispatch(this.fetch('posts', {
-      fetchOptions: { filters: { foo: 'bar' } },
-    }));
+  describe('default adapter', () => {
+    it('fetches the collection', function () {
+      const expectation = this.storageManager.stub('posts', { filters: { foo: 'bar' } });
+      this.store.dispatch(this.fetch('posts', {
+        fetchOptions: { filters: { foo: 'bar' } },
+      }));
 
-    expect(expectation.requestQueue.length).toBe(1);
+      expect(expectation.requestQueue.length).toBe(1);
+    });
+
+    describe('promise interface', () => {
+      it('exists', function () {
+        this.storageManager.stub('posts');
+        expect(this.store.dispatch(this.fetch('posts'))).toEqual(jasmine.any(Promise));
+      });
+
+      it('resolves with raw attributes when jqXHR is done', function (done) {
+        const postsModels = [new Post({ id: 1, title: 'Bar' }), new Post({ id: 2, title: 'Foo' })];
+        const expectation = this.storageManager.stub('posts', {
+          response(responseBody) {
+            responseBody.results = postsModels; // eslint-disable-line no-param-reassign
+          },
+        });
+        this.store.dispatch(this.fetch('posts')).then((response) => {
+          expect(response).toEqual(postsModels.map(model => model.attributes));
+          done();
+        });
+        expectation.respond();
+      });
+
+      it('rejects when jqXHR fails', function (done) {
+        const deferred = $.Deferred(); // eslint-disable-line new-cap
+        spyOn(this.storageManager.storage('posts'), 'fetch').and.returnValue(deferred);
+        this.store.dispatch(this.fetch('posts')).catch(done);
+        deferred.reject();
+      });
+    });
+
+    describe('previous XHRs', () => {
+      it('uses a trackKey to abort previous pending XHR', function () {
+        const abort = jasmine.createSpy('abort');
+        const state = () => 'pending';
+        spyOn(this.storageManager.storage('posts'), 'fetch').and.returnValue({ abort, state });
+        this.store.dispatch(this.fetch('posts', { trackKey: 'foo' })).catch(() => {});
+        this.store.dispatch(this.fetch('posts', { trackKey: 'foo' })).catch(() => {});
+
+        expect(abort).toHaveBeenCalledTimes(1);
+      });
+
+      it('does not abort non-pending previous XHR', function () {
+        const abort = jasmine.createSpy('abort');
+        const state = () => 'resolved';
+        spyOn(this.storageManager.storage('posts'), 'fetch').and.returnValue({ abort, state });
+        this.store.dispatch(this.fetch('posts', { trackKey: 'foo' })).catch(() => {});
+        this.store.dispatch(this.fetch('posts', { trackKey: 'foo' })).catch(() => {});
+
+        expect(abort).toHaveBeenCalledTimes(0);
+      });
+    });
   });
 
-  describe('promise interface', () => {
-    it('exists', function () {
-      this.storageManager.stub('posts');
-      expect(this.store.dispatch(this.fetch('posts'))).toEqual(jasmine.any(Promise));
-    });
-
-    it('resolves with raw attributes when jqXHR is done', function (done) {
-      const postsModels = [new Post({ id: 1, title: 'Bar' }), new Post({ id: 2, title: 'Foo' })];
-      const expectation = this.storageManager.stub('posts', {
-        response(responseBody) {
-          responseBody.results = postsModels; // eslint-disable-line no-param-reassign
-        },
-      });
-      this.store.dispatch(this.fetch('posts')).then((response) => {
-        expect(response).toEqual(postsModels.map(model => model.attributes));
-        done();
-      });
-      expectation.respond();
-    });
-
-    it('rejects when jqXHR fails', function (done) {
+  describe('when an adapter is passed in', () => {
+    it('uses the adapter', function () {
       const deferred = $.Deferred(); // eslint-disable-line new-cap
-      spyOn(this.storageManager.storage('posts'), 'fetch').and.returnValue(deferred);
-      this.store.dispatch(this.fetch('posts')).catch(done);
-      deferred.reject();
-    });
-  });
+      const stubAdapter = {
+        fetch: jasmine.createSpy('fetch').and.returnValue(deferred),
+      };
+      const fetchOptions = { filters: { foo: 'bar' } };
+      this.store.dispatch(this.fetch('posts', {
+        fetchOptions,
+        adapter: stubAdapter,
+      }));
 
-  describe('previous XHRs', () => {
-    it('uses a trackKey to abort previous pending XHR', function () {
-      const abort = jasmine.createSpy('abort');
-      const state = () => 'pending';
-      spyOn(this.storageManager.storage('posts'), 'fetch').and.returnValue({ abort, state });
-      this.store.dispatch(this.fetch('posts', { trackKey: 'foo' })).catch(() => {});
-      this.store.dispatch(this.fetch('posts', { trackKey: 'foo' })).catch(() => {});
-
-      expect(abort).toHaveBeenCalledTimes(1);
-    });
-
-    it('does not abort non-pending previous XHR', function () {
-      const abort = jasmine.createSpy('abort');
-      const state = () => 'resolved';
-      spyOn(this.storageManager.storage('posts'), 'fetch').and.returnValue({ abort, state });
-      this.store.dispatch(this.fetch('posts', { trackKey: 'foo' })).catch(() => {});
-      this.store.dispatch(this.fetch('posts', { trackKey: 'foo' })).catch(() => {});
-
-      expect(abort).toHaveBeenCalledTimes(0);
+      expect(stubAdapter.fetch).toHaveBeenCalledWith('posts', { fetchOptions });
     });
   });
 });
