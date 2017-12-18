efab96225d45afd744933a90100c641b4327ca82
Brandon Duff <bduff@mavenlink.com>
2017-12-18 15:59:29 -0700

    Sync Collections Reducer will merge new attributes for each model, not override
    - If we were refetching a collection, and our results omitted attributes our
      models already had, we were overriding the entire model and removing
      those attributes. This is not how the default Brainstem adapter behaves.
    
      Now, we iterate over each model in the result collection and merge on the
      new attributes with the existing models.
    
    Signed-off-by: Chris White <chris.white@codingzeal.com>

--- spec/reducers/sync-collections-spec.js
+++ spec/reducers/sync-collections-spec.js
@@ -1,24 +1,51 @@
 const syncCollections = require('../../lib/reducers/sync-collections.js');
 
 describe('syncCollections', () => {
-  const initialState = { users: { 1: 'user1', 2: 'user2' } };
+  beforeEach(function () {
+    this.initialState = { users: { 1: { name: 'user1' }, 2: { name: 'user2' } } };
+  });
+
   const collectionsToMerge = {
-    users: { 2: 'user20', 3: 'user3' },
-    workspaces: { 1: 'w1', 2: 'w1' },
+    users: { 2: { name: 'user20' }, 3: { name: 'user3' } },
+    workspaces: { 1: { name: 'w1' }, 2: { name: 'w1' } },
     roles: {},
   };
 
-  it('merges collections into the state', () => {
+  it('merges collections into the state', function () {
     const action = {
       payload: {
         collections: collectionsToMerge,
       },
     };
     const expectedCollections = {
-      users: { 1: 'user1', 2: 'user20', 3: 'user3' },
-      workspaces: { 1: 'w1', 2: 'w1' },
+      users: { 1: { name: 'user1' }, 2: { name: 'user20' }, 3: { name: 'user3' } },
+      workspaces: { 1: { name: 'w1' }, 2: { name: 'w1' } },
       roles: {},
     };
-    expect(syncCollections(initialState, action)).toEqual(expectedCollections);
+    expect(syncCollections(this.initialState, action)).toEqual(expectedCollections);
+  });
+
+  describe('when there are existing models in the collection', () => {
+    beforeEach(function () {
+      this.initialState = { users: { 1: { id: 1, oldAttribute: 'foo', newAttribute: 'bar' } } };
+    });
+
+    it('merges the new attributes onto the models', function () {
+      const action = {
+        payload: {
+          collections: {
+            users: {
+              1: { id: 1, newAttribute: 'baz' },
+            },
+          },
+        },
+      };
+
+      const expectedCollections = {
+        users: { 1: { id: 1, oldAttribute: 'foo', newAttribute: 'baz' } },
+      };
+
+      expect(syncCollections(this.initialState, action)).toEqual(expectedCollections);
+    });
   });
 });
