99b39a6f6c688896cdb0bbc2778c74a6aa924da2
Juan Carlos Medina <juanca@mavenlink.com>
2019-06-04 19:14:05 -0700

    Enforce `import/no-useless-path-segments`

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
   ],
   rules: {
     "function-paren-newline": "off",
-    "import/no-useless-path-segments": "off",
     "import/order": "off",
     "import/prefer-default-export": "off",
     "no-restricted-syntax": "off",
