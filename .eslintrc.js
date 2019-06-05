cc57b6a8410f698c34c67523a653606e1ee44bbe
Juan Carlos Medina <juanca@mavenlink.com>
2019-06-04 19:03:00 -0700

    Upgrade AirBnB ESLint configuration
    This should expose a linter rule against `export default { ... }` syntax and much more!
    
    At the moment, I turned off offending rules to get clean commits.
    
     Conflicts:
    	.eslintrc.js
    	package.json
    	yarn.lock

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,4 +5,17 @@ module.exports = {
     'jsx-a11y',
     'import',
   ],
+  rules: {
+    "comma-dangle": "off",
+    "function-paren-newline": "off",
+    "implicit-arrow-linebreak": "off",
+    "import/no-useless-path-segments": "off",
+    "import/order": "off",
+    "import/prefer-default-export": "off",
+    "no-restricted-syntax": "off",
+    "object-curly-newline": "off",
+    "operator-linebreak": "off",
+    "prefer-destructuring": "off",
+    "react/no-deprecated": "off",
+  },
 };
