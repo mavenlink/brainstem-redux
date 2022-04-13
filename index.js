c20a5ce95f3a7da63712fdc4f50d381d1c3b6240
Jared Nielson <jnielson@mavenlink.com>
2022-04-13 12:21:45 -0600

    Change import/export syntax to be jest friendly
    Co-authored-by: Ethan Anderson <eanderson@mavenlink.com>
    Co-authored-by: Chris Terry <cterry@mavenlink.com>

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-export { default as reducer } from './lib/reducers';
-export { default as updateStore } from './lib/sync/update-store';
-export { default as updateStorageManager } from './lib/middleware/update-storage-manager';
-export { default as makeBrainstemType } from './lib/types/make-brainstem-type';
-export {
+import reducer from './lib/reducers';
+import updateStore from './lib/sync/update-store';
+import updateStorageManager from './lib/middleware/update-storage-manager';
+import makeBrainstemType from './lib/types/make-brainstem-type';
+import stopUpdateStore from './lib/sync/stop-update-store';
+import { fetch as collectionFetch } from './lib/actions/collection';
+import {
   fetch as modelFetch,
   save as modelSave,
   destroy as modelDestroy,
   validate as modelValidate,
 } from './lib/actions/model';
-export { fetch as collectionFetch } from './lib/actions/collection';
-export { default as stopUpdateStore } from './lib/sync/stop-update-store';
+
+export {
+  reducer,
+  updateStore,
+  updateStorageManager,
+  makeBrainstemType,
+  stopUpdateStore,
+  collectionFetch,
+  modelFetch,
+  modelSave,
+  modelDestroy,
+  modelValidate,
+};
