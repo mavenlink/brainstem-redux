84f60fe7bf2d480013389505ac0d6232e46d59da
George Wang <gwang@mavenlink.com>
2024-04-02 11:14:12 -0700

    Spacing

--- example/reducers/posts-autocompleter.js
+++ example/reducers/posts-autocompleter.js
@@ -8,16 +8,16 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case 'POSTS_AUTOCOMPLETER_REQUEST_POSTS':
       return {
- ...state,
-filterText: action.payload.filterText,
+        ...state,
+        filterText: action.payload.filterText,
         isFetching: true,
-};
+      };
     case 'POSTS_AUTOCOMPLETER_SYNC_POSTS':
       return {
- ...state,
-isFetching: false,
+        ...state,
+        isFetching: false,
         posts: action.payload.posts,
-};
+      };
     default:
       return state;
   }
